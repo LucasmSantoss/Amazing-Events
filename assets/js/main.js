2ae459ab6a88ad6c164d55e11ef74cbd74e6bc37
Lucas Matias Santos <lucasmsantoss95@hotmail.com>
2023-02-06 11:28:39 -0300

    task2 v3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 const tarjets = document.getElementById("cards");
-let card = data.events;
+let cards = data.events;
 let boxCards = "";
 
-for (let cards of card) {
+for (let card of cards) {
   boxCards += `<div class="card p-3 m-1 bg-dark" style="width: 18rem">
     <img
-      src= ${cards.image}
+      src= ${card.image}
       class="card-img-top"
       style="height: 10rem"
       alt="cine"
     />
     <div class="card-body d-flex flex-column justify-content-around">
-            <h5 class="card-title text-white fs-4">${cards.name}</h5>
+            <h5 class="card-title text-white fs-4">${card.name}</h5>
             <p class="card-text text-white fs-5">
-              ${cards.description}
+              ${card.description}
             </p>
-            <p class="card-text text-white fs-5"> Price: $${cards.price} </p>
+            <p class="card-text text-white fs-5"> Price: $${card.price} </p>
             <a href="../assets/details.html" class="btn bg-dark-subtle d-flex justify-content-end " style="width: 4.5rem;"
               >Details</a
             >
@@ -24,3 +24,4 @@ for (let cards of card) {
 }
 
 tarjets.innerHTML = boxCards;
+
--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -1,23 +1,23 @@
 const tarjets = document.getElementById("upcoming-cards");
-let card = data.events;
+let cards = data.events;
 let boxCards = "";
 let currentDate = data.currentDate;
 
-for (let cards of card) {
-  if (cards.date > currentDate) {
+for (let card of cards) {
+  if (card.date > currentDate) {
     boxCards += `<div class="card p-3 m-1 bg-dark" style="width: 18rem">
         <img
-          src= ${cards.image}
+          src= ${card.image}
           class="card-img-top"
           style="height: 10rem"
           alt="cine"
         />
         <div class="card-body d-flex flex-column justify-content-around">
-                <h5 class="card-title text-white fs-4">${cards.name}</h5>
+                <h5 class="card-title text-white fs-4">${card.name}</h5>
                 <p class="card-text text-white fs-5"> ${cards.date} </p>
-                <p class="card-text text-white fs-5"> Price: $${cards.price} </p>
+                <p class="card-text text-white fs-5"> Price: $${card.price} </p>
                 <p class="card-text text-white fs-5">
-                  ${cards.description}
+                  ${card.description}
                 </p>
                 <a href="../assets/details.html" class="btn bg-dark-subtle d-flex justify-content-end " style="width: 4.5rem;"
                   >Details</a
